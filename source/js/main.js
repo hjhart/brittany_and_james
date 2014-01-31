695aa134e418fa3298b339bd6c73f446b05c0aa0
James Hart <hjhart@gmail.com>
2014-01-31 01:25:40 -0800

    Some javascripty stuff for incorrect answers.

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,26 @@
+$(document).ready(function() {
+
+    $('input').bind("keypress", {}, keypressInBox);
+
+    function keypressInBox(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13) { //Enter keycode
+            e.preventDefault();
+            var term = $('input').val()
+            if(isCorrectGuess(term)) {
+                console.log("YES!")
+            } else {
+                $('input').effect("shake", 300);
+            }
+        }
+    };
+
+    function isCorrectGuess(term) {
+        return false;
+    }
+
+    function showCard(index) {
+
+    }
+})
 
